328d0aebe451c01209a8b09225a5c53bf7420e3a
FTC6567RoboRaiders <smkocik23@yahoo.com>
2018-10-28 14:49:53 -0400

    Fixed auto sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStormSampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStormSampleAuto.java
@@ -6,25 +6,28 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import RedStorm.Robot.Robot;
 
-@Autonomous (name="Sample RedStorm Autonomous Program", group="Samples")
+@Autonomous (name="Sample Auto", group="Samples")
 
 /**
  * Created by Steve Kocik as a sample for RedStorm to build off of...
  */
 
-public abstract class RedStormSampleAuto extends LinearOpMode {
+public class RedStormSampleAuto extends LinearOpMode {
 
 
     public Robot robot = new Robot();    // Create a new instance of the robot
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
 
 
         // Initialize and set up the robot's drive motors
         robot.initialize(hardwareMap);             // Initialize the robot
         robot.resetEncoders();                     // Reset the encoder counts
         robot.runWithEncoders();                   // Tell the motors to run with encoders
+
+        telemetry.addData("Status ", "Initialized");
+        telemetry.update();
 
         // Wait for the start button to be pushed
         waitForStart();
@@ -36,10 +39,18 @@ public abstract class RedStormSampleAuto extends LinearOpMode {
 
         // While the autonomous period is still active AND the robot has not reached the number
         // of encoder counts to travel 24 inches
-        while(opModeIsActive() && robot.getDriveEncoderCount() < encoderCountstoTravel) {
+        while(opModeIsActive()) {
 
-            robot.setDriveMotorPower(0.5, 0.50);   // Set power to 50%
+            telemetry.addData("Status ", "opModeIsActive");
+
+            if (robot.getDriveEncoderCount() < encoderCountstoTravel) {
+                robot.setDriveMotorPower(0.5, 0.50);   // Set power to 50%
+                telemetry.addData("encoderCount", "(%.0f)",robot.getDriveEncoderCount());
+            }
+
+            telemetry.update();
         }
+
 
         robot.setDriveMotorPower(0.0,0.0);         // Motors stop
     }
